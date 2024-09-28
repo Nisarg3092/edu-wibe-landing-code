46ca315b6cda450f0877073c222bfc9405b5402c
Nisarg Vaghela <145669514+Nisarg3092@users.noreply.github.com>
2024-09-29 00:40:57 +0530

    Add files via upload

---
+++ edu wibe landing code/Js/script.js
@@ -0,0 +1,9 @@
+hm_burger = document.querySelector('.ham-burger')
+nav_hight = document.querySelector('.nav')
+
+
+hm_burger.addEventListener('click', ()=>{
+
+    nav_hight.classList.toggle('nav-hight');
+
+})
